cbb7129d9986c7be954e987f85658d4050c80afb
zhu xiaochun <zhu.xiaochun@qq.com>
2025-04-11 17:55:34 +0800

    发布了： 1.0.16 版本。

--- src/apiResponseThenable.ts
+++ src/apiResponseThenable.ts
@@ -1,8 +1,9 @@
 
-import {Thenable} from '@baoxia/utils.javascript';
+import { Thenable } from '@baoxia/utils.javascript';
 import type { ApiResponseInfo } from './apiResponseInfo.js';
+import { AxiosError } from 'axios';
 
-export class ApiResponseThenable<ResponseParamType> 
-    extends Thenable<string | null, ApiResponseInfo<ResponseParamType>>
-{}
+export class ApiResponseThenable<ResponseParamType>
+	extends Thenable<AxiosError | null | any, ApiResponseInfo<ResponseParamType>>
+{ }
 export default ApiResponseThenable;
